f98feff655df0451f7ce14a88db8c7d7efaa50ac
Rodrigo594 <rodrigo.rodrigues68@gmail.com>
2019-05-12 18:03:52 -0300

    Revert "msm8996-common: Import configpanel"
    This reverts commit df6656db8b675fac1eeade96f1f2e3ced0ddd83c.

--- msm8996.mk
+++ msm8996.mk
@@ -144,10 +144,6 @@ PRODUCT_PACKAGES += \
     init.wlan.sh \
     ueventd.qcom.rc
 
-# ConfigPanel
-PRODUCT_PACKAGES += \
-    ConfigPanel
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
