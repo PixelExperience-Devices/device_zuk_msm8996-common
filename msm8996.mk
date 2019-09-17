40816b0f021d3affd5416e05beb14de03ec1183c
kubersharma001 <kubersharma001@gmail.com>
2019-09-17 15:06:26 +0530

    msm8996-common: prebuilt: [DNM][TEMP] Use OpenCamera for now
     * This is just a temporary change because Camera2 is very glitchly and randomly crashes, so until that is fixed lets use this.
    
     * More Info:
     https://sourceforge.net/p/opencamera/code/
     https://opencamera.sourceforge.io/
     https://sourceforge.net/projects/opencamera/files/
    
    Change-Id: Ibd4a78db4457b432e166f114f0bb8d5e56378b9f

--- msm8996.mk
+++ msm8996.mk
@@ -136,7 +136,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8996 \
     mm-qcamera-app \
-    libmm-qcamera
+    libmm-qcamera \
+    opencamera
 
 # CNE
 PRODUCT_PACKAGES += \
