bda4203636fd6b96e25acfde0b8e622b679cd64c
Rodrigo594 <rodrigo.rodrigues68@gmail.com>
2019-09-28 20:14:42 +0000

    msm8996-common: Remove unused libqsap_sdk & libQWiFiSoftApCfg

--- msm8996.mk
+++ msm8996.mk
@@ -399,8 +399,6 @@ PRODUCT_PACKAGES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    libqsap_sdk \
-    libQWiFiSoftApCfg \
     libwpa_client \
     hostapd \
     wpa_supplicant \
